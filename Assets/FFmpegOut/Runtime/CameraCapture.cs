69b32acea66d0087e2ac15f2884a6aac918351fc
EdoardoBettazzi <edoardobettazzi1@gmail.com>
2024-05-13 16:50:43 +0200

    Solved recorder timestamps

--- Assets/FFmpegOut/Runtime/CameraCapture.cs
+++ Assets/FFmpegOut/Runtime/CameraCapture.cs
@@ -14,14 +14,14 @@ namespace FFmpegOut
     {
         #region Public properties
 
-        [SerializeField] int _width = 1280;
+        [SerializeField] int _width = 1920;
 
         public int width {
             get { return _width; }
             set { _width = value; }
         }
 
-        [SerializeField] int _height = 720;
+        [SerializeField] int _height = 1080;
 
         public int height {
             get { return _height; }
@@ -68,6 +68,9 @@ namespace FFmpegOut
         float _startTime;
         int _frameDropCount;
 
+        // ADDED BY EDO
+        int _frameDelay;
+
         float FrameTime {
             get { return _startTime + (_frameCount - 0.5f) / _frameRate; }
         }
@@ -77,7 +80,7 @@ namespace FFmpegOut
             if (++_frameDropCount != 10) return;
 
             Debug.LogWarning(
-                "Significant frame dropping was detected. This may introduce " +
+                "Significant frame droppping was detected. This may introduce " +
                 "time instability into output video. Decreasing the recording " +
                 "frame rate is recommended."
             );
@@ -155,17 +158,18 @@ namespace FFmpegOut
             }
 
             #endregion
+
         }
 
         IEnumerator Start()
+        {
+            // Sync with FFmpeg pipe thread at the end of every frame.
+            for (var eof = new WaitForEndOfFrame(); ;)
             {
-                // Sync with FFmpeg pipe thread at the end of every frame.
-                for (var eof = new WaitForEndOfFrame();;)
-                {
-                    yield return eof;
-                    _session?.CompletePushFrames();
-                }
+                yield return eof;
+                _session?.CompletePushFrames();
             }
+        }
 
         void Update()
         {
@@ -174,13 +178,15 @@ namespace FFmpegOut
             #region ADDED BY EDO: SAVE RECORDER FRAMES
 
             GameObject experiment = GameObject.Find("Experiment");
-            int frame_num = experiment.GetComponent<MainTask>().frame_number;
+            int main_frame_num = experiment.GetComponent<MainTask>().frame_number;
+            int last_state = experiment.GetComponent<MainTask>().last_state;
             long main_start_time = experiment.GetComponent<MainTask>().starttime;
             int reward_count = experiment.GetComponent<Ardu>().reward_counter;
 
             // Check if the StreamWriter is not initialized
             if (!_isStreamWriterInitialized)
             {
+
                 // Get path_to_data and lastIDFromDB from the Saver
                 string path_to_MEF = experiment.GetComponent<Saver>().path_to_MEF;
                 int lastIDFromDB = experiment.GetComponent<Saver>().lastIDFromDB;
@@ -188,10 +194,10 @@ namespace FFmpegOut
                 // Check if path_to_data and lastIDFromDB are not null or zero
                 if (!string.IsNullOrEmpty(path_to_MEF) && lastIDFromDB != 0)
                 {
-                    path_to_data_RecorderFrames = Path.Combine(path_to_MEF, "VIDEO", (DateTime.Now.ToString("yyyy_MM_dd") 
-                        + "_ID" + (lastIDFromDB + 1).ToString() + $"_{camera.tag}" + "_recorderFrames.csv"));
+                    path_to_data_RecorderFrames = Path.Combine(path_to_MEF, "VIDEO",
+                        (DateTime.Now.ToString("yyyy_MM_dd") + "_ID" + (lastIDFromDB + 1).ToString() + $"_{camera.tag}" + "_recorderFrames.csv"));
                     _streamWriter = new StreamWriter(path_to_data_RecorderFrames);
-                    _streamWriter.WriteLine("Timestamp,Frame,Reward_count");
+                    _streamWriter.WriteLine("Timestamp,Frame_time,Rec_Frame,Droppings,Dropped_frames,Reward_count,Current_state");
 
                     // Set the flag to true after initializing the StreamWriter
                     _isStreamWriterInitialized = true;
@@ -199,16 +205,16 @@ namespace FFmpegOut
             }
 
             #endregion
-            
+
             // Lazy initialization
-            if (_session == null) // && frame_num == 10) ADDED BY EDO TO CONTROL START OF RECORDING 
+            if (_session == null)
             {
                 // Give a newly created temporary render texture to the camera
                 // if it's set to render to a screen. Also create a blitter
                 // object to keep frames presented on the screen.
                 if (camera.targetTexture == null)
                 {
-                    _tempRT = new RenderTexture(_width, _height, 24, GetTargetFormat(camera)); 
+                    _tempRT = new RenderTexture(_width, _height, 24, GetTargetFormat(camera));
                     _tempRT.antiAliasing = GetAntiAliasingLevel(camera);
                     camera.targetTexture = _tempRT;
                     _blitter = Blitter.CreateInstance(camera);
@@ -226,56 +232,81 @@ namespace FFmpegOut
                 _frameCount = 0;
                 _frameDropCount = 0;
 
+                // ADDED BY EDO
+                _frameDelay = 0;
             }
-            
+
             var gap = Time.time - FrameTime;
             var delta = 1 / _frameRate;
 
-            if (frame_num > 10) // ADDED BY EDO TO CONTROL START OF RECORDING 
+            if (gap < 0)
             {
-                if (gap < 0)
-                {
-                    // Update without frame data.
-                    _session.PushFrame(null);
-                }
-                else if (gap < delta)
-                {
-                    // Single-frame behind from the current time:
-                    // Push the current frame to FFmpeg.
-                    _session.PushFrame(camera.targetTexture);
-                    _frameCount++;
-                }
-                else if (gap < delta * 2)
-                {
-                    // Two-frame behind from the current time:
-                    // Push the current frame twice to FFmpeg. Actually this is not
-                    // an efficient way to catch up. We should think about
-                    // implementing frame duplication in a more proper way. #fixme
-                    _session.PushFrame(camera.targetTexture);
-                    _session.PushFrame(camera.targetTexture);
-                    _frameCount += 2;
-                }
-                else
-                {
-                    // Show a warning message about the situation.
-                    WarnFrameDrop();
-
-                    // Push the current frame to FFmpeg.
-                    _session.PushFrame(camera.targetTexture);
-
-                    // Compensate the time delay.
-                    _frameCount += Mathf.FloorToInt(gap * _frameRate);
-                }
+                // Update without frame data.
+                _session.PushFrame(null);
+            }
+            else if (gap < delta)
+            {
+                // Single-frame behind from the current time:
+                // Push the current frame to FFmpeg.
+                _session.PushFrame(camera.targetTexture);
+                _frameCount++;
 
                 // ADDED BY EDO: After pushing the frame to FFmpeg, write the details to the CSV file.
-                if (_isStreamWriterInitialized)
-                {
-                    long rec_time = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                    _streamWriter.WriteLine($"{(rec_time - main_start_time)},{_frameCount},{reward_count}");
-                }
+                addRecorderFrameRow(main_start_time, reward_count, last_state);
 
             }
+            else if (gap < delta * 2)
+            {
+                // Two-frame behind from the current time:
+                // Push the current frame twice to FFmpeg. Actually this is not
+                // an efficient way to catch up. We should think about
+                // implementing frame duplication in a more proper way. #fixme
+                _session.PushFrame(camera.targetTexture);
+                _frameCount += 1;
 
+                // ADDED BY EDO: After pushing the frame to FFmpeg, write the details to the CSV file.
+                addRecorderFrameRow(main_start_time, reward_count, last_state);
+
+                _session.PushFrame(camera.targetTexture);
+                _frameCount += 1;
+
+                // ADDED BY EDO: After pushing the frame to FFmpeg, write the details to the CSV file.
+                addRecorderFrameRow(main_start_time, reward_count, last_state);
+
+            }
+            else
+            {
+                // Show a warning message about the situation.
+                WarnFrameDrop();
+
+                // Push the current frame to FFmpeg.
+                _session.PushFrame(camera.targetTexture);
+
+                // Compensate the time delay.
+                _frameCount += Mathf.FloorToInt(gap * _frameRate);
+
+                // ADDED BY EDO: After pushing the frame to FFmpeg, write the details to the CSV file
+
+                _frameDelay += Mathf.FloorToInt(gap * _frameRate);
+
+                addRecorderFrameRow(main_start_time, reward_count, last_state);
+            }
+
+        }
+
+        #endregion
+
+
+        #region ADDED BY EDO: Methods
+
+        private void addRecorderFrameRow(long main_start_time, int reward_count, int last_state)
+        {
+            // ADDED BY EDO: After pushing the frame to FFmpeg, write the details to the CSV file.
+            if (_isStreamWriterInitialized)
+            {
+                long rec_time = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                _streamWriter.WriteLine($"{(rec_time - main_start_time)},{FrameTime},{_frameCount},{_frameDropCount},{_frameDelay},{reward_count},{last_state}");
+            }
         }
 
         #endregion
